99efe6e07209076565b95a3656474822b013731a
NatasjaAndersen <31003506+NatasjaA@users.noreply.github.com>
2017-12-13 12:24:01 +0100

    test

--- hifi_api/routes/produkter.js
+++ hifi_api/routes/produkter.js
@@ -42,83 +42,83 @@ module.exports = function (app) {
 
     //..........................................ADMIN CRUD AF PRODUKTER.................................
 
-    // app.post('/produkt', security.isAuthenticated, (req, res, next) => { // Route som uploader billeder 
+    app.post('/produkter', security.isAuthenticated, (req, res, next) => { // Route som uploader billeder 
         
-    //             let image = 'no-image.png';
+                let image = 'no-image.png';
         
-    //             let sql = `INSERT INTO produkter SET navn=?,pris=?,beskrivelse=?,fk_kategori_id=?,fk_producent=?, billede=?`;
+                let sql = `INSERT INTO produkter SET navn=?,pris=?,beskrivelse=?,fk_kategori_id=?,fk_producent=?, billede=?`;
         
-    //             let name = (req.body.navn == undefined ? '' : req.body.navn);
-    //             let description = (req.body.beskrivelse == undefined ? '' : req.body.beskrivelse);
-    //             let price = (req.body.pris == undefined ? 0 : req.body.pris);
-    //             let kategori_id = req.body.kategori_id;
-    //             let producent_id = req.body.producent_id;
-    //             price = price.replace(',', '.');
-    //             if (name != '' && description != '' && !isNaN(price)) {
-    //                 // håndter billedet, hvis der er sendt et billede 
-    //                 if (req.files.billede.name != '') {
-    //                     image = req.files.billede.name;
+                let name = (req.body.navn == undefined ? '' : req.body.navn);
+                let description = (req.body.beskrivelse == undefined ? '' : req.body.beskrivelse);
+                let price = (req.body.pris == undefined ? 0 : req.body.pris);
+                let kategori_id = req.body.kategori_id;
+                let producent_id = req.body.producent_id;
+                price = price.replace(',', '.');
+                if (name != '' && description != '' && !isNaN(price)) {
+                    // håndter billedet, hvis der er sendt et billede 
+                    if (req.files.billede.name != '') {
+                        image = req.files.billede.name;
         
-    //                     // flyt den uploadede midlertidige fil til billede mappen
-    //                     var temp_image = fs.createReadStream('./' + req.files.billede.path); // input stream
-    //                     var final_image = fs.createWriteStream('./images/' + image); // output stream
-    //                     temp_image.pipe(final_image); // flyt data fra temp til final
-    //                     temp_image.on('end', function () {
-    //                         // slet den midlertidige fil, når "final_image" er oprettet  
-    //                         fs.unlink('./' + req.files.billede.path);
-    //                     });
-    //                 } else {
-    //                     // denne er nødvendig, pga en tom fil bliver lagt i uploadmappen hver gang formularen sendes.
-    //                     fs.unlink('./' + req.files.billede.path);
-    //                 }
+                        // flyt den uploadede midlertidige fil til billede mappen
+                        var temp_image = fs.createReadStream('./' + req.files.billede.path); // input stream
+                        var final_image = fs.createWriteStream('./images/' + image); // output stream
+                        temp_image.pipe(final_image); // flyt data fra temp til final
+                        temp_image.on('end', function () {
+                            // slet den midlertidige fil, når "final_image" er oprettet  
+                            fs.unlink('./' + req.files.billede.path);
+                        });
+                    } else {
+                        // denne er nødvendig, pga en tom fil bliver lagt i uploadmappen hver gang formularen sendes.
+                        fs.unlink('./' + req.files.billede.path);
+                    }
         
-    //                 console.log(name, price, description, kategori_id, producent_id, image);
-    //                 db.query(sql, [name, price, description, kategori_id, producent_id, image], function (err, data) {
-    //                     if (err) {
-    //                         console.log(err);
-    //                     } else {
-    //                         res.json(200, data);
-    //                     }
-    //                 })
-    //             } else {
-    //                 res.json(400, {
-    //                     message: 'validering fejlede'
-    //                 });
-    //             }
-    //     });
+                    console.log(name, price, description, kategori_id, producent_id, image);
+                    db.query(sql, [name, price, description, kategori_id, producent_id, image], function (err, data) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            res.json(200, data);
+                        }
+                    })
+                } else {
+                    res.json(400, {
+                        message: 'validering fejlede'
+                    });
+                }
+        });
 
-    //     app.get('/images/:name', (req, res, next) => { // Route som gør at uploadede billeder midlertidigt kommer i temp mappen
-    //         // det er kun jpg eller png filer jeg ønsker at tillade adgang til her
-    //         if (path.extname(req.params.name) == '.jpg' || path.extname(req.params.name) == '.png' || path.extname(req.params.name) == '.gif') {
-    //             // forsøg at læs billede filen fra images mappen...
-    //             fs.readFile('./images/' + req.params.name, function (err, file) {
-    //                 if (err) {
-    //                     // den ønskede fil blev ikke fundet, vi sender standard "no-image.png" i stedet
-    //                     // dette kunne også have været en res.json(404) 
-    //                     fs.readFile('./images/no-image.png', (err2, default_file) => {
-    //                         res.writeHead(200);
-    //                         res.write(default_file);
-    //                         res.end();
-    //                     });
+        app.get('/images/:name', (req, res, next) => { // Route som gør at uploadede billeder midlertidigt kommer i temp mappen
+            // det er kun jpg eller png filer jeg ønsker at tillade adgang til her
+            if (path.extname(req.params.name) == '.jpg' || path.extname(req.params.name) == '.png' || path.extname(req.params.name) == '.gif') {
+                // forsøg at læs billede filen fra images mappen...
+                fs.readFile('./images/' + req.params.name, function (err, file) {
+                    if (err) {
+                        // den ønskede fil blev ikke fundet, vi sender standard "no-image.png" i stedet
+                        // dette kunne også have været en res.json(404) 
+                        fs.readFile('./images/no-image.png', (err2, default_file) => {
+                            res.writeHead(200);
+                            res.write(default_file);
+                            res.end();
+                        });
     
-    //                 } else {
-    //                     // her kunne der skaleres "on-the-fly" ... det tager vi en anden dag
-    //                     res.writeHead(200);
-    //                     res.write(file);
-    //                     res.end();
-    //                 }
-    //             });
-    //         } else {
-    //             // hvis den ønskede fil ikke er en .jpg eller .png, 
-    //             // sendes no-image som standard eller res.json(404)
-    //             res.json(404);
-    //             // fs.readFile('./api/images/no-image.png', (err, default_file) => {
-    //             //    res.writeHead(200);
-    //             //    res.write(default_file);
-    //             //    res.end();
-    //             // });
-    //         }
-    //     });
+                    } else {
+                        // her kunne der skaleres "on-the-fly" ... det tager vi en anden dag
+                        res.writeHead(200);
+                        res.write(file);
+                        res.end();
+                    }
+                });
+            } else {
+                // hvis den ønskede fil ikke er en .jpg eller .png, 
+                // sendes no-image som standard eller res.json(404)
+                res.json(404);
+                // fs.readFile('./api/images/no-image.png', (err, default_file) => {
+                //    res.writeHead(200);
+                //    res.write(default_file);
+                //    res.end();
+                // });
+            }
+        });
     
 
 
@@ -209,30 +209,30 @@ module.exports = function (app) {
     // });
 
 
-    app.post('/oprett', security.isAuthenticated, function (req, res, next) {
+    // app.post('/oprett', security.isAuthenticated, function (req, res, next) {
 
-                let navn = req.body.navn;
-                let pris = req.body.pris;
-                let beskrivelse = req.body.beskrivelse;
-                let fk_kategori_id = req.body.fk_kategori_id;
-                let fk_producent = req.body.fk_producent;
-                let billede = req.body.billede;
-        console.log(navn);
+    //             let navn = req.body.navn;
+    //             let pris = req.body.pris;
+    //             let beskrivelse = req.body.beskrivelse;
+    //             let fk_kategori_id = req.body.fk_kategori_id;
+    //             let fk_producent = req.body.fk_producent;
+    //             let billede = req.body.billede;
+    //     console.log(navn);
 
-               let sql = `INSERT INTO produkter(id, navn, pris, beskrivelse, fk_kategori_id, fk_producent, billede) 
-               VALUES (null, ?,?,?,?,?,?)`;
+    //            let sql = `INSERT INTO produkter(id, navn, pris, beskrivelse, fk_kategori_id, fk_producent, billede) 
+    //            VALUES (null, ?,?,?,?,?,?)`;
 
-               db.query(sql,[navn,pris,beskrivelse,fk_kategori_id,fk_producent,billede], function (err, data) {
+    //            db.query(sql,[navn,pris,beskrivelse,fk_kategori_id,fk_producent,billede], function (err, data) {
 
-                   if (err) {
-                        console.log(err);
-                    } else {
-                        res.send("Ok");
+    //                if (err) {
+    //                     console.log(err);
+    //                 } else {
+    //                     res.send("Ok");
 
-                    }
-                })
+    //                 }
+    //             })
 
-           });
+    //        });
 
     //.............................OPRET KATEGORI.......................
 
