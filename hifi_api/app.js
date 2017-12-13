dfb7aa570412dd7cf5720bf9f60d134fd3b4610f
NatasjaAndersen <31003506+NatasjaA@users.noreply.github.com>
2017-12-13 12:21:29 +0100

    test

--- hifi_api/app.js
+++ hifi_api/app.js
@@ -9,12 +9,12 @@ const server = restify.createServer({
 server.use(logger('dev'));
 server.use(restify.plugins.acceptParser(server.acceptable));
 // bodyparser skal vide hvor billederne skal placeres 
-server.use(restify.plugins.bodyParser({
-    mapParms: true,
-    mapFiles: true,
-    keepExtensions: true,
-    uploadDir: './temp'
-}));
+// server.use(restify.plugins.bodyParser({
+//     mapParms: true,
+//     mapFiles: true,
+//     keepExtensions: true,
+//     uploadDir: './temp'
+// }));
 server.use(restify.plugins.bodyParser());
 const cors = corsmiddleware({ origins: ['*'],allowHeaders: ['Authorization', 'userID'] });
 server.pre(cors.preflight);
